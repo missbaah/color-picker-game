a518412ddebb6ab9b0978e2a758b24b78baf812f
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-02-07 10:59:50 +0000

    color picker composable

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -8,29 +8,14 @@
   </button>
 </template>
 
-<script>
-import { ref, reactive } from "@vue/reactivity";
+<script setup>
+import useColorPicker from '../composables/color-picker';
 
-export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
+const { colors, message, matchColor } = useColorPicker();
 
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
 
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
-  },
-};
 </script>
 
-<style></style>
+<style>
+
+</style>
